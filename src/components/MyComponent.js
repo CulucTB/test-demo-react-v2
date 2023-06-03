6f122e4f936459d35abdfb0c70f5c995f76fd4c7
Hoi Dan IT <nguyenvanluc21051999@gmail.com>
2023-06-03 09:01:32 +0900

    vide016

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// class component
+// function component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    // JXS cho phep viet  trong html trong js   
+    render () {
+        return (
+            <div> my first component </div>
+        );
+
+    }
+}
+
+export default MyComponent;
