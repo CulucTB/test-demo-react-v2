781292affd2d3a829f2fa7d2c6a6283a20d49218
Hoi Dan IT <nguyenvanluc21051999@gmail.com>
2023-06-03 10:06:02 +0900

    video19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,13 +10,23 @@ class MyComponent extends React.Component {
         address: 'Hoi Dan It',
         age: 26
     };
-    // JXS cho phep viet  trong html trong js
 
+    handleClick(event) {
+        console.log(">> click me my button")
+        console.log(" My name is", this.state.name)
+    }
+
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
+    // JXS cho phep viet  trong html trong js
 
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onMouseOver={this.handleOnMoverOver}> Hover </button>
+                <button onClick={this.handleClick}>   Click me  </button>
             </div>
         );
 
