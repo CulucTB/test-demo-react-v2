90b562b9cf52dba72ef772a95c8723b5154e8173
Hoi Dan IT <nguyenvanluc21051999@gmail.com>
2023-06-04 06:46:01 +0900

    video27

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -15,12 +15,11 @@ class MyComponent extends React.Component {
     }
 
     handleAddNewUser = (userObj) => {
-        console.log(">>> check data from parent: ", userObj)
         this.setState({
             // listUsers: [userObj, ...this.state.listUsers]
             listUsers: [...this.state.listUsers, userObj]
         })
-
+        //khi để userObj ở đầu thì list sẽ hiện ở đầu ,còn ở cuối thì list sẽ hiện ở cuối 
     }
     // JXS cho phep viet  trong html trong js
     // DRY :don't repeat youseft
